971815831f29a012d34fea10610ea18884e2b651
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-11 14:48:25 -0700

    add update functionality for the firestore db

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -61,13 +61,9 @@ function TicketControl() {
     setEditing(true);
   }
 
-  const handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = mainTicketList
-    // new code: selectedTicket.id
-    .filter(ticket => ticket.id !== selectedTicket.id)
-    .concat(ticketToEdit);
-    setMainTicketList(editedMainTicketList);
-    // new code!
+  const handleEditingTicketInList = async (ticketToEdit) => {
+    const ticketRef = doc(db, "tickets", ticketToEdit.id);
+    await updateDoc(ticketRef, ticketToEdit);
     setEditing(false);
     setSelectedTicket(null);
   }
