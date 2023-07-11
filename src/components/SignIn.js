0d130833ac65b25628b67c69b1020a6e66c498b7
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-11 16:18:08 -0700

    add user sign in page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "./../firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignIn(){  
+  // New code below!
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // New code below!
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
+      })
+      .catch((error) => {
+        // New code below!
+        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
+      });
+  }
+
   return (
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign up</h1>
+      {/* New code below! */}
+      {signUpSuccess}
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+    </React.Fragment>
   );
 }
 
